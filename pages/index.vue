4e06ada1dce911a41c33e05c9b62e9e90d381f67
James Pickard <james.pickard@gmail.com>
2021-08-12 17:16:10 +0100

    Update index and add assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="open-sans">This text is supposed to be Open Sans</div>
+    <div class="image" style="margin-top: 1em">
+      Here's an image of what Open Sans is supposed to look like:
+      <img src="~/assets/open-sans-sample.png" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
